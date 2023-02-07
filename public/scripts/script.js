975761bbf8955537af6ef44a3ac368a94f8926f4
AbdelmouanaimHmamed <mounaimhmamed@gmail.com>
2023-02-07 16:58:39 +0100

    make it better

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,8 +6,14 @@ let closeButton = document.getElementById("close");
 openButton.addEventListener("click" , openNav );
 function openNav(){
     sectionToappear.style.right = 0;    
+    if (screen.width <= 400){
+        sectionToappear.style.left = 0;
+    }
 }
 
 closeButton.addEventListener("click" , () => {
     sectionToappear.style.right = '-400px';
+    if (screen.width <= 400){
+        sectionToappear.style.left = '-400px'
+    }
 })
