ec30df52a5a91441c596010fa2491415393c9020
AbdelmouanaimHmamed <mounaimhmamed@gmail.com>
2023-02-09 16:38:46 +0100

    make it better

--- routes/projects.js
+++ routes/projects.js
@@ -6,21 +6,21 @@ var Projects = [
         id : 1 ,
         projectName : "IRIC ARCHIVE",
         projectsubTitleName : "web Development ",
-        link : "/home",
+        link : "https://github.com/Abdelmounaimhmamed/IRIC-archive",
         imagesrc : "https://esmartech.ae/webroot/front/images/Node-Js.png"
     },
     {
         id : 1 ,
         projectName : "Electron App",
         projectsubTitleName : "Desktop development ",
-        link : "/home",
+        link : "https://github.com/Abdelmounaimhmamed/electron-desktop-app-",
         imagesrc : "https://res.cloudinary.com/practicaldev/image/fetch/s--03FhpCOu--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/l8ro3v0chfxaxgsseb1a.png"
     },
     {
         id : 1 ,
         projectName : "Php App",
         projectsubTitleName : "Web Application",
-        link : "/home",
+        link : "https://github.com/Abdelmounaimhmamed/php-student-management-html-css-php-mysql-",
         imagesrc : "https://www.interviewbit.com/blog/wp-content/uploads/2021/12/PHP-Projects-1160x653.png"
     }
 ]
